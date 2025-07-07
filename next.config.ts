c0451186a57c4b8ffe6f6c3d74b85dfd1feffdc6
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-07-07 15:16:32 +0300

    up

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "test-next",
   images: {
-    path: "/"
+    unoptimized: true
   }
 };
 
