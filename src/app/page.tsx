b7fd56219571729829b94c3b706a9fa4c594bac6
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-07-07 15:32:19 +0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import logo from "../../public/next.svg";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="./next.svg"
+          src={logo}
           alt="Next.js logo"
           width={180}
           height={38}
