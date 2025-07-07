a3a0bc044f26c9b90da5fdd1c8e0faacb11765d3
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-07-07 16:12:22 +0300

    up

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  basePath: '/text-next/',
-  assetPrefix: '/text-next/',
+  output: "export",
+  basePath: '/text-next',
+  assetPrefix: '/text-next',
 };
 
 export default nextConfig;
