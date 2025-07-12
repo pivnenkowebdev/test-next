79adcc95126e11a850eae3f2992b32f81bcd2d88
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-07-12 10:36:55 +0300

    up

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/test-next',
-  images: { unoptimized: true },
+  // basePath: '/test-next',
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import logo from "./images/next.svg"
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src={logo}
+          src="./next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
